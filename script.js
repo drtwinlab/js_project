12786bc0a59c60ab951faf39aa2c869294e66425
drtwinlab <56961525+drtwinlab@users.noreply.github.com>
2025-11-29 13:18:09 +0100

    Initial commit of the project files

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
-let zname = "Admin";
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let userprompt = prompt("Камень, ножницы или бумага? Введи слово!");
+
+userprompt = getHumanChoice(userprompt);
+let aiprompt = getMachineChoice();
+
+//Points
+let userpoint = 0;
+let aipoint = 0;
+
+function getHumanChoice(userInput){
 
 
-if (zname === "Admin") {
-  let pass = "TheMaster";
-            if (pass === "TheMaster") {
-                console.log("Welcome")
-            } else if (pass === "" || pass === null){console.log("Canceled")}
-            else {console.log("Wrong password")}
+userInput = userInput.toLowerCase().trim();
 
-} else if (zname === "" || zname === null) {
-    console.log("Cancelled")
-} else {console.log("Idk")}
+return userInput
+}
+
+// AI Logic
+function getMachineChoice() {
+let pcprompt = getRandomIntInclusive(1, 3);
+
+if (pcprompt == 1) {
+    pcprompt = "Камень";
+} else if (pcprompt == 2) {
+    pcprompt = "Ножницы";
+} else pcprompt = "Бумага";
+
+return pcprompt.toLowerCase().trim();
+}
+
+
+
+function playRound(userprompt, aiprompt) {
+    if ((userprompt == "бумага") && (aiprompt == "камень")) {
+    console.log("Победил юзер!"); return userpoint++;
+    } else if ((userprompt == "ножницы") && (aiprompt == "камень")){
+        console.log("Победил ИИ!"); return aipoint++;
+            } else if ((userprompt == "бумага") && (aiprompt == "ножницы")){
+            console.log("Победил ИИ!"); return aipoint++;
+                } else if ((userprompt == "камень") && (aiprompt == "ножницы")){
+                console.log("Победил юзер!"); return userpoint++;
+                    } else if ((userprompt == "камень") && (aiprompt == "бумага")){
+                    console.log("Победил ИИ!"); return aipoint++;
+                        } else if ((userprompt == "ножницы") && (aiprompt == "бумага")){
+                        console.log("Победил юзер!"); return userpoint++;
+                        } else console.log("Ничья!");
+
+}
+
+let Round = 5;
+for (let i =1; i<Round; i++ ) {
+console.log("Round: "+i)
+playRound(userprompt, aiprompt);
+}
+
